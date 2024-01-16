4931ef61cad045d7c6f2feadaba7f3e8788ddf09
Jasper-Hewitt <jasper.hewitt@me.com>
2024-01-16 10:05:09 +0800

    push

--- text_mining.r
+++ text_mining.r
@@ -115,18 +115,16 @@ master_fert_word <- master_fert_df %>%
 
 
 
-#same custom stopwords as we use for the wisenews wordcloud
-custom_stopwords <- c('告訴', '願意', '推動', '承諾', '這片', '再次', '最大', '中央', '重要', '到底', '建設','期待', 
-                        '帶給', '趙少康', '選舉', '立刻', '必須', '清德', '清楚', 
+custom_stopwords <- c('支持', '告訴', '願意', '推動', '承諾', '這片', '再次', '最大', '中央', '重要', '到底', '建設','期待', 
+                        '帶給', '趙少康', '選舉', '立刻', '必須', '清德', '清楚', '卻是', '主張', 
                        '變成', '絕對', '區域', '侯友', '選擇', '更是', '主席', '不能', '確是', '還給',  
                        "執政", "總統", "國民黨", "2024", "政治", "如今", "在此", '最後', '全國', "賴清德", "中華民國", "宜來", "未來", "侯友宜", "政黨", "一定", "民眾", "就讓",
-                       "民進黨", "人民", "政府", "國家", "經濟", "科技", "報導", "可能", "指出", "認為", "新聞網", "國際", 
+                       "民進黨", "人民", "政府", "國家", "報導", "可能", "指出", "認為", "新聞網", "國際", 
                       "應該", "可能", "提出", "過去", "現在", "進行","今天", "相關", "社會",
                       "議題", "很多", "undo", "需要", "需求", "已經", "目前", "今年", "透過",
                       "地方", "沒有", "記者", "成為", "持續", "市場", "表示", "台灣", "造成",
-                      "不少", "原因", "影響", "人口","台北", "生育率", "問題", "育兒", "生育", 
-                      "少子化", "/", "10", "20", "30", "一起", "桃園", "台中", "市長", "市民", "朋友",
-                      "城市", "12", "11", "高雄", "https", "台北市", "台中市", "台南市", "高雄市", "台南")  # specific words about 生育率# specific words about 生育率
+                      "不少", "原因", "影響", "問題", "/", "10", "20", "30", "一起", "市長", "市民", "朋友",
+                      "城市", "12", "11", "https")  # specific words about 生育率# specific words about 生育率
 stopwords <- c(stopwords, custom_stopwords)
 
 #___________# WORDCLOUD AND WORD FREQUENCY LIST #__________# 
@@ -159,3 +157,91 @@ ggplot(top_20_words, aes(x = reorder(word, n), y = n)) +
   labs(x = "Words", y = "Count", title = "20 most frequent words in candidates' \n posts about fertility") +
   theme_minimal() +
   theme(text = element_text(family = "Songti SC", size = 20))
+
+
+#___________# TOPIC MODELLING #__________# 
+#test topic modelling
+# Required libraries
+library(dplyr)
+library(jiebaR)
+library(tm)
+library(topicmodels)
+library(tidytext)
+
+#import full crowdtangle csv 8073 rows
+master_df <- read.csv("/Users/jasperhewitt/Desktop/github_repos/Campaigning_101/data/all_attacks.csv")
+
+#import xlsx with additional data for each candidate (party, city, english name, etc.)
+#candidate_info <- read_xlsx("/Users/jasperhewitt/Desktop/fertnews/candidates_info.xlsx")
+
+#only keep relevant columns 
+master_df <- master_df %>%
+  select('serial_number', 'User.Name', 'combined_text', 'attack', 'target')
+
+#filter so User.Name == houyuih
+master_df <- master_df %>%
+  filter(User.Name == 'houyuih')
+
+
+#in the combined_text column, only keep the sentences that contain the target. these are the interpunction that may be used
+# 。 ？ ！ 
+
+
+search_pattern <- "民進黨|賴|蔡|綠營"
+
+master_df <- master_df %>%
+  mutate(important_sentences = str_split(combined_text, "[。？！]")) %>% #split it into sentences
+  #find the sentences that contain some of our keywords, and paste that sentences into the important_sentences column
+  mutate(important_sentences = map(important_sentences, ~ .[str_detect(., regex(search_pattern, ignore_case = TRUE))])) %>%
+  #unnest the columns, so each important sentence will get their own row. just like 'explode in pandas' 
+  unnest(important_sentences)
+
+unique_serial_numbers <- length(unique(master_df$serial_number))
+cat("Number of unique serial numbers:", unique_serial_numbers, "\n")
+
+
+
+# For word segmentation only
+my_seg <- worker(bylines = F, user = "customdict.txt", symbol = F)
+
+# Load Chinese stopwords
+stopwords <- readLines("stopwords_zh_trad.txt", encoding = "UTF-8")
+custom_stopwords <- c('告訴', '願意', '推動', '承諾', '這片', '再次', '最大', '中央', '重要', '到底', '建設','期待', 
+                        '帶給', '趙少康', '選舉', '立刻', '必須', '清德', '清楚', 
+                       '變成', '絕對', '區域', '侯友', '選擇', '更是', '主席', '不能', '確是', '還給',  
+                       "執政", "總統", "國民黨", "2024", "政治", "如今", "在此", '最後', '全國', "賴清德", "中華民國", "宜來", "未來", "侯友宜", "政黨", "一定", "民眾", "就讓",
+                       "民進黨", "人民", "政府", "國家", "報導", "可能", "指出", "認為", "新聞網", "國際", 
+                      "應該", "可能", "提出", "過去", "現在", "進行","今天", "相關", "社會",
+                      "議題", "很多", "undo", "需要", "需求", "已經", "目前", "今年", "透過",
+                      "地方", "沒有", "記者", "成為", "持續", "市場", "表示", "台灣", "造成",
+                      "不少", "原因", "影響", "問題", "/", "10", "20", "30", "一起", "市長", "市民", "朋友",
+                      "城市", "12", "11", "https")  # specific words about 生育率# specific words about 生育率
+stopwords <- c(stopwords, custom_stopwords)
+
+# Function to remove one-character words
+remove_one_char_words <- function(word_list){
+  word_list[sapply(word_list, function(word) nchar(word) > 1)]
+}
+
+# Segmentation and removal of stopwords
+master_df$seg_content <- sapply(master_df$important_sentences, function(row) {
+  words <- segment(code = row, jiebar = my_seg)
+  words <- words[!words %in% stopwords]
+  remove_one_char_words(words)
+})
+
+# Create Corpus from the content column
+corpus <- Corpus(VectorSource(master_df$seg_content))
+
+# Document Term Matrix
+dtm <- DocumentTermMatrix(corpus)
+
+# Run LDA
+k <- 4  # Number of topics
+lda_model <- LDA(dtm, k)
+
+# Get the terms from the model
+terms(lda_model, 10)  # Returns the top 15 terms for each topic
+
+# Posterior probabilities
+posterior <- posterior(lda_model)
